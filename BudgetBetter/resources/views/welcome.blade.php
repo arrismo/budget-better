06ed2887f17acbfa4a6bd5dbb6e3e2144c181899
Arris Moise <46009365+arrismo@users.noreply.github.com>
2020-07-14 23:28:51 -0400

    footer

--- BudgetBetter/resources/views/welcome.blade.php
+++ BudgetBetter/resources/views/welcome.blade.php
@@ -401,8 +401,48 @@
                         </div>
                     </div>
                 </div>
+                <!-- Footer -->
+<footer class="page-footer font-small unique-color-dark pt-4">
+
+    <!-- Footer Elements -->
+    <div class="container">
+  
+      <!-- Call to action -->
+      <ul class="list-unstyled list-inline text-center py-2">
+        <li class="list-inline-item">
+          <h5 class="mb-1">Register for free</h5>
+        </li>
+        <li class="list-inline-item">
+          <a href="/register" class="btn btn-outline-white btn-rounded">Sign up!</a>
+        </li>
+      </ul>
+      <!-- Call to action -->
+  
+    </div>
+    <!-- Footer Elements -->
+  
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">© 2020 Copyright:
+      BudgetBetter
+    </div>
+    <!-- Copyright -->
+  
+  </footer>
+  <!-- Footer -->
             </div>
             <!-- end of contact us slide-->
+ 
+
+
+
+
+
+
+
+
+
+
+
         </div>
 
         <div id="social-icons">
@@ -416,7 +456,7 @@
         </div>
     </div>  
 
-       
+  
 
             
 
