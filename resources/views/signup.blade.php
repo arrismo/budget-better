656810d76cf45b3af65608697b03b4c029a40d08
Arris Moise <mikaelmoise@protonmail.com>
2020-07-25 10:15:51 -0400

    csrf

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -151,7 +151,7 @@
   <body>
     <div class="testbox">
       <form method="POST" action="{{ route('page_3') }}">
-        
+        @csrf
 
         <div class="banner">
           <h1>Sign-Up</h1>
