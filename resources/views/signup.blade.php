b5d86f8e3702d1289959ed323dd9bba6266f6bf7
Arris Moise <mikaelmoise@protonmail.com>
2020-07-28 19:08:30 -0400

    signup ui change

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -5,6 +5,11 @@
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <style>
+      body {
+        background: #f12711; 
+        background: -webkit-linear-gradient(to right,#f5af19,#f12711); 
+        background: linear-gradient(to right,#f5af19,#f12711); 
+      }
       html, body {
       min-height: 100%;
       }
