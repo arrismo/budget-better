036412e62cf813c0ae7e7be963ab9b6e26b5241c
Arris Moise <mikaelmoise@protonmail.com>
2020-07-30 11:25:05 -0400

    nav bars

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,17 @@
 
 	<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
- 
+ <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+
+
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
      <link  type="text/css" rel="stylesheet" href="http://budget-better.com/css/dboard.css">
 
 
@@ -22,7 +32,7 @@
 <body>
 <nav class="navbar navbar-expand-md  nav-color ">
         <div class="container">
-            <a class="" href="{{ url('/') }}" style="color: #fff; font-size:30px; ">
+            <a class="" href="{{ url('/') }}" style="font-size:30px; ">
                 {{ config('app.name', 'BudgetBetter') }}
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
@@ -37,7 +47,7 @@
                
                     <!-- Authentication Links -->
                         <li class="nav-item dropdown"> 
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" style="color:white" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle"  role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                               Welcome  {{ Auth::user()->name }} <span class="caret"></span>
                             </a>
 
@@ -144,7 +154,7 @@
 
 
 <div class="row">
-    	<div class="col-sm-6">
+    	<div class="col-sm-4">
     		<div class="card shadow">
             	<div class="card-header py-3">
                   <h6 class="m-0 font-weight-bold text-primary">Development Approach</h6>
@@ -157,7 +167,7 @@
         </div>
 
 
-        <div class="col-sm-6">
+        <div class="col-sm-4">
         	<div class="card shadow">
                 <div class="card-header py-3">
                   <h6 class="m-0 font-weight-bold text-primary">Development Approach</h6>
@@ -169,6 +179,7 @@
 	</div>
 </div>
 
+</div>
 
 <div style="color:#818c35;text-align:center" class="footer-copyright text-center py-3">© 2020 Copyright:
       BudgetBetter
