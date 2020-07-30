9b1a8aa41cf029f751589690ef4446b4b1508231
Arris Moise <mikaelmoise@protonmail.com>
2020-07-30 13:14:43 -0400

    routing

--- routes/web.php
+++ routes/web.php
@@ -36,14 +36,17 @@ Route::get('/signup', function () {
 
 
 
-Route::get('/resources/views/signup', 'PagesController@sp')->name('signup');
+Route::get('/resources/views/dashboard', 'PagesController@db')->name('dashboard');
+
+
+Route::post('/resources/views/signup', 'PagesController@sp')->name('signup');
 
 
 Route::get('/resources/views/page_3', 'PagesController@p3')->name('page_3');
 
 Route::get('/resources/views/page_4', 'PagesController@p4')->name('page_4');
 
-Route::get('/resources/views/dashboard', 'PagesController@db')->name('dashboard');
+
 
 
 
