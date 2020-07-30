730a462c75503c07a303e86ee854c508beefa82e
FIRST_NAME LAST_NAME <eileenasinc@eileens-air.myfiosgateway.com>
2020-07-29 21:35:10 -0400

    navbar

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -1,11 +1,5 @@
 <!DOCTYPE html>
 <html>
-<head>
-  @extends('layouts.app')
-
-  @section('content')
-</head>
-  
     <title>Sign-Up</title>
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
@@ -157,6 +151,7 @@
         border-image: initial;
       }
     </style>
+
   <body>
     <div class="testbox">
       <form  action="{{ route('page_3') }}">
