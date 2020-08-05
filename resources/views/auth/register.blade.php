658afbd9ab573de67d2042d2896e1937a5523eef
FIRST_NAME LAST_NAME <eileenasinc@eileens-air.myfiosgateway.com>
2020-08-04 21:12:06 -0400

    fix error message

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -87,6 +87,7 @@
                             <input id="password-confirm" type="password" class="input100 @error('password') is-invalid @enderror" name="password_confirmation" autocomplete="new-password">
                             @error('password')
                                     <span class="invalid-feedback" role="alert">
+                                    </br>
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
