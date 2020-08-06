13e753896ff1bc5f79ec8f0615759b699ab69cc2
FIRST_NAME LAST_NAME <eileenasinc@eileens-air.myfiosgateway.com>
2020-08-05 21:42:11 -0400

    added navbar to signup part2

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -29,24 +29,7 @@
                     </a>
                     </li>
                
-                    <!-- Authentication Links -->
-                        <li class="nav-item dropdown"> 
-                            <a id="navbarDropdown" style="color:white;" class="nav-link dropdown-toggle"  role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                              Welcome  {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
+                    
 
 
                       
