acdfefb9787ffeb925755a9925a41ac8ef46d96d
Neamt <nsabry@smith.edu>
2020-08-09 00:30:31 -0400

    tryinng to fix email not found bug

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -10,14 +10,14 @@ use App\Http\Controllers\Controller;
 class MailController extends Controller {
    public function html_email(Request $request) {
       $name = $request->input('name');
-      $email = $request->input('email');
+      // $email = $request->input('email');
       $message = $request->input('message');
       $data = array('name'=>$name, 'msg'=>$message);
 
       Mail::send('mail', $data, function($message) {
          $message->to('budgetbetter1@gmail.com', 'BudgetBetter')->subject
             ('Email sent from website');
-         $message->from($email, $name);
+         $message->from('hhh@abc.com', $name);
       });
 
       echo "HTML Email Sent. Check your inbox.";
