0060f4bee34853805645e1a29577b6cbf0977fab
Neamt <nsabry@smith.edu>
2020-08-10 00:31:10 -0400

    forgot a $ in web.blade

--- resources/views/mail.blade.php
+++ resources/views/mail.blade.php
@@ -1,6 +1,6 @@
 <p>Hello!</p>
 <p>You were sent an email through the BudgetBetter website conntact section</p>
-<p>Name: {{ name }}</p>
+<p>Name: {{ $name }}</p>
 <p>Email: {{ $email }}</p>
 <p>Message: {{ $msg }}</p>
 <p>Thank you!</p>
