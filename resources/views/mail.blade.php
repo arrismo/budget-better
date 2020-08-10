f1a429b2d857cf06109f66c789d83fe9049ee8da
Neamt <nsabry@smith.edu>
2020-08-10 00:34:01 -0400

    just one more edit to message conntetn and i'll stop i promise

--- resources/views/mail.blade.php
+++ resources/views/mail.blade.php
@@ -1,5 +1,5 @@
 <p>Hello!</p>
-<p>You were sent an email through the BudgetBetter website conntact section</p>
+<p>You were sent an email through the BudgetBetter website conntact section. Here are the details:</p>
 <p>Name: {{ $name }}</p>
 <p>Email: {{ $email }}</p>
 <p>Message: {{ $msg }}</p>
