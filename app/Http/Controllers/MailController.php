625aa7955fcc5b000e5326fd0190381d1637a09b
Neamt <nsabry@smith.edu>
2020-08-10 00:20:58 -0400

    reverting back to when it was workingn

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Controller;
 
 class MailController extends Controller {
    public function html_email(Request $request) {
+<<<<<<< HEAD
 
       $name = $request->input('name');
       $email = $request->input('email');
@@ -19,6 +20,18 @@ class MailController extends Controller {
          $message->to('budgetbetter1@gmail.com', 'BudgetBetter')->subject
             ('Email sent from website');
          $message->from('website', 'website');
+=======
+      // $user = new User;
+      // $msg = $request->input('name');
+      $msg = $request->input('email');
+      $data = array('name'=>'ME','msg'=>$msg);
+
+
+      Mail::send('mail', $data, function($message) {
+         $message->to('budgetbetter1@gmail.com', 'BudgetBetter')->subject
+            ('Email sent from website');
+         $message->from('abc@email.com', 'meeee');
+>>>>>>> parent of 10be54d1... should be workinng at this pooint
       });
 
       echo "HTML Email Sent. Check your inbox.";
