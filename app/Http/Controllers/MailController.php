5b738dc453073153239efa4f86cf871229b86f0d
Neamt <nsabry@smith.edu>
2020-08-09 23:50:02 -0400

    trying other thinngs with mail conntroller

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -10,7 +10,7 @@ use App\Http\Controllers\Controller;
 class MailController extends Controller {
    public function html_email(Request $request) {
       // $user = new User;
-      $msg = $request->getContent();
+      $msg = $request->name;
       $data = array('name'=>'ME','msg'=>$msg[0]);
 
 
