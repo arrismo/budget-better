d83dfe30f2644899a3e3f33afe91464bd2323071
Neamt <nsabry@smith.edu>
2020-08-18 19:08:55 -0400

    make pages.blade process python script instead

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -11,8 +11,22 @@ class PagesController extends Controller
 		return view('page_3');
 	}
 
-	public function p4()
-	{
+	public function runScript(){
+        $process = new Process(['python', 'app/Http/Controllers/scrapinngScript.py']);
+        $process->run();
+
+        // what's going on y'all
+
+        // executes after the command finishes
+        if (!$process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }
+
+        echo $process->getOutput(); 
+    }
+
+	public function p4() {
+		runScript();
 		return view('page_4');
 	}
 
