442124b8bd879b2ef300ede712aa3e5b4f6c3eaa
Neamt <nsabry@smith.edu>
2020-08-18 19:22:19 -0400

    imported process in pages

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+
+
 class PagesController extends Controller
 {
 	public function p3()
