fedcfd2ea15ecda72fc66e84d8ac726ab1975449
Arris Moise <46009365+arrismo@users.noreply.github.com>
2020-08-27 17:22:24 -0400

    routing

--- resources/views/page_4.blade.php
+++ resources/views/page_4.blade.php
@@ -8,7 +8,7 @@
   <title>Budget Better</title>
   <link rel="stylesheet" href="{{asset('css/page_4.css')}}">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>    
-  <script type="text/javascript" src="{{ URL::asset('js/data.js') }}"></script>
+  <script type="text/javascript" src="{{ URL::asset('resources/js/data.js') }}"></script>
 
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet"> 
