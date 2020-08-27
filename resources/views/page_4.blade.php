8a1bc552e46965fbb460eab1f3139c0e98aff91c
Arris Moise <46009365+arrismo@users.noreply.github.com>
2020-08-27 16:02:24 -0400

    took out process from input

--- resources/views/page_4.blade.php
+++ resources/views/page_4.blade.php
@@ -72,8 +72,8 @@
             <ul class="list-group list-group-flush">
               
                 <li class="list-group-item text-left">Food
-                  <input  placeholder="{{ $process }}"  style=" background-color:#fff; width:25%;  border: 1px solid black; border-radius: .60rem; margin-left: 170px;"    class="inline" aria-label="Small" aria-describedby="inputGroup-sizing-sm">  
-                  {{ $process }}
+                  <input    style=" background-color:#fff; width:25%;  border: 1px solid black; border-radius: .60rem; margin-left: 170px;"    class="inline" aria-label="Small" aria-describedby="inputGroup-sizing-sm">  
+                 
                 </li>
 
                 <li class="list-group-item text-left">Rent
