fa12e210df85a14ea0e2361e33c6edd31a81afb4
Neamt <nsabry@smith.edu>
2020-08-30 14:58:13 -0400

    never minnd

--- app/Http/Controllers/GoutteController.php
+++ app/Http/Controllers/GoutteController.php
@@ -18,7 +18,7 @@ class GoutteController extends Controller
         $goutteClient->setClient($guzzleClient);
         $crawler = $goutteClient->request('GET', 'https://www.expatistan.com/cost-of-living/madrid');
         $prices = array();
-        $crawler->filter('.price .city-1')->each(function ($node) {
+        $crawler->filter('.price')->each(function ($node) {
             global $prices;
 
             // replace all the commas 
