ddde97e6b2f1e04f5e75a182143b45148a9a38c9
Neamt <nsabry@smith.edu>
2020-08-30 00:24:07 -0400

    cho instead

--- app/Http/Controllers/GoutteController.php
+++ app/Http/Controllers/GoutteController.php
@@ -18,7 +18,7 @@ class GoutteController extends Controller
         $goutteClient->setClient($guzzleClient);
         $crawler = $goutteClient->request('GET', 'https://www.expatistan.com/cost-of-living/madrid');
         $crawler->filter('.price')->each(function ($node) {
-            dump($node->text());
+            echo $node->text();
         });
     }
 }
