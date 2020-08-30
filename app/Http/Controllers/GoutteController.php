c6998cc75292520d29d3d2d698baa4a7c40c8704
Neamt <nsabry@smith.edu>
2020-08-30 12:53:59 -0400

    just no commas

--- app/Http/Controllers/GoutteController.php
+++ app/Http/Controllers/GoutteController.php
@@ -18,7 +18,7 @@ class GoutteController extends Controller
         $goutteClient->setClient($guzzleClient);
         $crawler = $goutteClient->request('GET', 'https://www.expatistan.com/cost-of-living/madrid');
         $crawler->filter('.price')->each(function ($node) {
-            $noCommas = str_replace(",", "", $node->text()."\n");
+            $noCommas = strval(str_replace(",", "", $node->text()."\n"));
             echo "<script>console.log($noCommas)</script>";
         });
     }
