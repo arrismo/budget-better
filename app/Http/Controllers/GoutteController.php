6021468cf77d88108ee9346e2b023ed9dd81a3c9
Neamt <nsabry@smith.edu>
2020-08-30 00:39:50 -0400

    and replace

--- app/Http/Controllers/GoutteController.php
+++ app/Http/Controllers/GoutteController.php
@@ -18,7 +18,7 @@ class GoutteController extends Controller
         $goutteClient->setClient($guzzleClient);
         $crawler = $goutteClient->request('GET', 'https://www.expatistan.com/cost-of-living/madrid');
         $crawler->filter('.price')->each(function ($node) {
-            print substr(($node->text()."\n"), 1, strlen($node->text()));
+            print str_replace(",", "", substr(($node->text()."\n"), 1));
         });
     }
 }
