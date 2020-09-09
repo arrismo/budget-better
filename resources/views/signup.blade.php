5fac6fb950747f1c5fbb7670ab63066778d0d473
FIRST_NAME LAST_NAME <eileenasinc@eileens-air.myfiosgateway.com>
2020-09-08 20:24:50 -0400

    changed signup pt5

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -50,7 +50,7 @@
         </div>
     </nav>
     <title>Sign-Up</title>
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+    <!-- <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet"> -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <style>
       body {
