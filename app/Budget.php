5eb3f675b5df9e2dc08cc21263a22583e7be2466
Arris Moise <46009365+arrismo@users.noreply.github.com>
2020-11-28 21:06:01 -0500

    show budget

--- app/Budget.php
+++ app/Budget.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Budget extends Model
 {
     protected $table = 'user_budget';
+    
     protected $fillable = 
     ['rent','spending','transportation','food','other'];
     
