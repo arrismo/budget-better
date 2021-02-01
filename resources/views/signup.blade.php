ed5d454bfcce1657bbe0ebe61ce9fb5df0d2c95c
FIRST_NAME LAST_NAME <eileenasinc@Eileens-MacBook-Air.local>
2021-02-01 17:14:56 -0500

    ui for signup pt2

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-<!-- <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet"> -->
+<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet"> 
  <!-- Stylesheets -->
  <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
         <link rel="stylesheet" href="{{asset('css/font-awesome.css')}}">
