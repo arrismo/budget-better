abe3335a85171c2ffcaeb11281f3afc6924d1a34
FIRST_NAME LAST_NAME <eileenasinc@Eileens-MacBook-Air.local>
2021-02-01 17:19:30 -0500

    ui for signup pt6

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -14,7 +14,7 @@
         <link rel="stylesheet" href="{{asset('css/util.css')}}">
         <link rel="stylesheet" href="{{asset('css/main.css')}}">
 
-<!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 
@@ -27,7 +27,7 @@
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> 
 <script src="//geodata.solutions/includes/countrystatecity.js"></script>
 
-<link href="{{ URL::asset('vendor/fontawesome-free/all.min.css') }}" rel="stylesheet" type="text/css" >
+<!-- <link href="{{ URL::asset('vendor/fontawesome-free/all.min.css') }}" rel="stylesheet" type="text/css" >
 
 <link href="{{ URL::asset('css/dboard.css') }}" rel="stylesheet" type="text/css" > -->
 <head>
