4f8b916b1538a5f4fa0e6286f6a855443f9c39b7
arris <mikaelmoise@protonmail.com>
2021-03-02 15:42:11 -0500

    fixing budget display

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -38,44 +38,44 @@ class PagesController extends Controller{
     // }
 
 	public function p4($city) {
-        // // start scrappinng data from website
-		// $goutteClient = new Client();
-        // $guzzleClient = new GuzzleClient(array(
-        //     'timeout' => 60,
-        //     'verify' => false
-        // ));
-        // $goutteClient->setClient($guzzleClient);
+        // start scrappinng data from website
+		$goutteClient = new Client();
+        $guzzleClient = new GuzzleClient(array(
+            'timeout' => 60,
+            'verify' => false
+        ));
+        $goutteClient->setClient($guzzleClient);
 
-        // // echo 'https://www.expatistan.com/cost-of-living/' . strtolower(strval($city));
-        // $crawler = $goutteClient->request('GET', 'https://www.expatistan.com/cost-of-living/' . strtolower(strval($city)));
-        // $prices = array();
-        // $crawler->filter('.city-1')->each(function ($node) {
-        //     global $prices;
-        //     $nodeText = $node->text();
+        // echo 'https://www.expatistan.com/cost-of-living/' . strtolower(strval($city));
+        $crawler = $goutteClient->request('GET', 'https://www.expatistan.com/cost-of-living/' . strtolower(strval($city)));
+        $prices = array();
+        $crawler->filter('.city-1')->each(function ($node) {
+            global $prices;
+            $nodeText = $node->text();
 
-        //     // replace all the commas 
-        //     $noCommas = str_replace(",", "", $node->text()."\n");
+            // replace all the commas 
+            $noCommas = str_replace(",", "", $node->text()."\n");
             
-        //     global $resultStr;
+            global $resultStr;
 
-        //     $numStr = $this->extractNums($noCommas, $resultStr);
+            $numStr = $this->extractNums($noCommas, $resultStr);
 
-        //     // add float version to array
-        //     $prices[] = floatval($numStr);
-        // });
+            // add float version to array
+            $prices[] = floatval($numStr);
+        });
 
-        // global $prices;
-        // global $currency;
+        global $prices;
+        global $currency;
 
 
-        // $foodSum = $prices[1] + $prices[3] + $prices[4] + $prices[5] + $prices[6] + $prices[7] + $prices[8] + $prices[9];
-        // $housingSum = $prices[18] +  $prices[19] +  $prices[20] +  $prices[23];
-		// $spendSum =  $prices[43] +  $prices[47] +  $prices[51] +  $prices[25];
-        // $travelSum =  $prices[31];
-        // $otherSum =  $prices[33] +  $prices[36] +  $prices[37] +  $prices[38] +  $prices[39] +  $prices[40];
+        $foodSum = $prices[1] + $prices[3] + $prices[4] + $prices[5] + $prices[6] + $prices[7] + $prices[8] + $prices[9];
+        $housingSum = $prices[18] +  $prices[19] +  $prices[20] +  $prices[23];
+		$spendSum =  $prices[43] +  $prices[47] +  $prices[51] +  $prices[25];
+        $travelSum =  $prices[31];
+        $otherSum =  $prices[33] +  $prices[36] +  $prices[37] +  $prices[38] +  $prices[39] +  $prices[40];
         
-      //  return view('page_4', ["foodSum"=>number_format($foodSum), "housingSum"=>number_format($housingSum), "spendSum"=>number_format($spendSum), "travelSum"=>number_format($travelSum), "otherSum"=>number_format($otherSum)]);
-        return view('page_4', ["foodSum"=>number_format(200), "housingSum"=>number_format(200), "spendSum"=>number_format(200), "travelSum"=>number_format(200), "otherSum"=>number_format(200)]);
+        return view('page_4', ["foodSum"=>number_format($foodSum), "housingSum"=>number_format($housingSum), "spendSum"=>number_format($spendSum), "travelSum"=>number_format($travelSum), "otherSum"=>number_format($otherSum)]);
+       // return view('page_4', ["foodSum"=>number_format(200), "housingSum"=>number_format(200), "spendSum"=>number_format(200), "travelSum"=>number_format(200), "otherSum"=>number_format(200)]);
 
 	}
 
